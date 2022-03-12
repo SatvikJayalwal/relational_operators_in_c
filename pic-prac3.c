b67c99e7905a2418d2cdb51d0eb779d7df5951b2
Satvik Jayalwal <69150647+SatvikJayalwal@users.noreply.github.com>
2022-03-12 15:48:21 +0530

    Add files via upload

---
+++ pic-prac3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    printf("\nThis program is a Demonstration of Relational operations in C langusge\n");
+    int x = 12, y = 13;
+    printf("x = %d\n", x);
+    printf("y = %d\n\n", y);
+    printf("x > y : %d\n", x > y);    
+    printf("x >= y : %d\n", x >= y);   
+    printf("x < y : %d\n", x < y);   
+    printf("x <= y : %d\n", x <= y);    
+    printf("x == y : %d\n", x == y);
+    printf("x != y : %d\n", x != y);
+    return 0;
+}
